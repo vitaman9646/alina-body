2a76fde1d9749e1f4556dba73a0b6072e18107a5
vitaman9646 <mrvit9646@gmail.com>
2026-08-30 11:45:53 +0700

    Update import paths for db-client and utils

--- api/progress.js
+++ api/progress.js
@@ -1,5 +1,5 @@
-import supabase from './db-client.js';
-import { setCors, getUserFromReq, findEnrollment } from './utils.js';
+import supabase from '../lib/db-client.js';
+import { setCors, getUserFromReq, findEnrollment } from '../lib/utils.js';
 
 export default async function handler(req, res) {
   setCors(res);
