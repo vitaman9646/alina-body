99e47634f16dfc7baa88dd23eb24ff1cf51c6a3d
vitaman9646 <mrvit9646@gmail.com>
2026-08-30 11:44:03 +0700

    Update import paths for db-client and utils

--- api/orders.js
+++ api/orders.js
@@ -1,5 +1,5 @@
-import supabase from './db-client.js';
-import { setCors, getUserFromReq } from './utils.js';
+import supabase from '../lib/db-client.js';
+import { setCors, getUserFromReq } from '../lib/utils.js';
 
 export default async function handler(req, res) {
   setCors(res);
